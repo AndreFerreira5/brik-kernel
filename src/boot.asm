6e66276e741728a62e05681dadd53510efdaea54
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2023-08-20 23:04:31 +0100

    boot.asm fixed, kernel now boots

--- src/boot.asm
+++ src/boot.asm
@@ -1,12 +1,15 @@
 bits 32
 
+extern main
+global start
+
+ALIGN 4
 section .multiboot
-        dd 0x1BADB002
-        dd 0x0
+        dd 0x1BADB002   ; Magic number
+        dd 0x0          ; Flags
+        dd -(0x1BADB002 + 0x0)  ; Checksum (magic + flags)
 
 section .text
-global start
-extern main
 
 start:
         cli
